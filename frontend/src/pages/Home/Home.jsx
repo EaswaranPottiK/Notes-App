68debfecc7ef7cbf3d0f7cceabc85e01d4bb0bf3
Easwaran <easwaranpottiak@gmail.com>
2024-11-26 11:42:52 +0530

    dashbord complete

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import NoteCard from '../../components/Cards/NoteCard'
 import { IoMdAdd } from "react-icons/io";
 import AddEditNotes from './AddEditNotes';
+import Modal from 'react-modal'
+
 
 const Home = () => {
+  const [openEditModel, setOpenEditModel] = useState({
+    isShown:false,
+    type:"add",
+    data:null,
+})
+
   return (
     <>
       <Navbar/>
@@ -22,10 +30,29 @@ const Home = () => {
           />
         </div>
       </div>
-      <div className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10'>
+      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10'
+        onClick={()=>{setOpenEditModel({isShown:true, type:'add',data:null})}}
+        >
           <IoMdAdd className='text-[32px] text-white'/>
-      </div>
-      <AddEditNotes/>
+      </button>
+      <Modal
+       isOpen={openEditModel.isShown}
+       onRequestClose={()=>{}}
+       style={{
+          overlay:{
+            backgroundColor:"rgba(0,0,0,0.2)",
+          },
+
+       }}
+       contentLabel=""
+       className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-hidden"
+       >
+        <AddEditNotes 
+        type={openEditModel.type}
+        data={openEditModel.data}
+        onClose={()=>{setOpenEditModel({isShown:false, type:'add',data:null})}}
+        />
+      </Modal>
     </>
   )
 }
